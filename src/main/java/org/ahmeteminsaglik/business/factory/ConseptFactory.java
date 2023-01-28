0f59238861f9e758254a5f06b2f5cd8c4c2375c4
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2023-01-28 17:36:53 +0300

    removed buildWordProcessConsept function. Created jar file.

--- src/main/java/org/ahmeteminsaglik/business/factory/ConseptFactory.java
+++ src/main/java/org/ahmeteminsaglik/business/factory/ConseptFactory.java
@@ -3,9 +3,7 @@ package org.ahmeteminsaglik.business.factory;
 import org.ahmeteminsaglik.MemoryUsage;
 import org.ahmeteminsaglik.Stopwatch;
 import org.ahmeteminsaglik.entity.ComplexityConsept;
-import org.ahmeteminsaglik.entity.WordProcessConsept;
 import org.ahmeteminsaglik.enums.EnumProcessName;
-import org.ahmeteminsaglik.enums.EnumWordTable;
 
 public class ConseptFactory {
     public ComplexityConsept buildComplexityConsept(EnumProcessName processName, Stopwatch stopwatch, MemoryUsage memoryUsage) {
@@ -13,15 +11,6 @@ public class ConseptFactory {
         consept.setEnumProcessName(processName);
         consept.setStopwatch(stopwatch);
         consept.setMemoryUsage(memoryUsage);
-        return consept;
-    }
-
-    public WordProcessConsept buildWordProcessConsept(EnumWordTable enumTotalWordList, EnumWordTable enumSearchWordList, int foundWord, int missingWord) {
-        WordProcessConsept consept = new WordProcessConsept();
-        consept.setEnumTotalWordList(enumTotalWordList);
-        consept.setEnumSearchWordList(enumSearchWordList);
-        consept.setFoundWord(foundWord);
-        consept.setMissingWord(missingWord);
         return consept;
     }
 }
